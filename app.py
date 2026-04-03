cfa2dcd010a6154924ee5f4dccab6da565d0057d
Mohammad Irfan <mohammedirfan7799897786@gmail.com>
2026-04-03 12:56:36 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        task = request.form.get("task")
+        if task:
+            tasks.append(task)
+        return redirect("/")
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/delete/<int:index>")
+def delete(index):
+    if index >=0 and index < len(tasks):
+        tasks.pop(index)
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False)
